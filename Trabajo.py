019c9132c01757514cd0b6d93514455e1c8b3bb3
ToxicArea450 <carlos.r.g.18@gmail.com>
2018-04-25 21:40:30 +0200

    [Modificaciones en casa]

--- Trabajo.py
+++ Trabajo.py
@@ -6,59 +6,39 @@ import matplotlib.pyplot as plt
 from math import pi, atan
 
 #Parámetros del problema
-R=7.5e10-14
-zs=1.0e-21
-qe=1.6e-19
-u=1.66e-27 
-e0=8.08542e-12
+R, zs, qe, u, e0= 7.5e-14, 1.0e-21, 1.6e-19, 1.66e-27, 8.08542e-12
 
 #Datos de las cargas y masas
 Q=79*qe         #Carga del núclero de oro
 q=2*qe          #Carga de la partícula alfa (núcleo de Helio)
-M=4*u          #Masa de la partícula alfa 
+m=4*u          #Masa de la partícula alfa 
 
-cte=(q*Q)/(M*4*pi*e0)
-#Plantear las derivadas
-def derivadas(var,t): 
-	x, y, vx, vy= var[0], var[1], var[2], var[3] #Los valores iniciales vendrán en el array var
-	ax=cte*(x/(x**2+y**2)**(3./2))
-	ay=cte*(y/(x**2+y**2)**(3./2))
+cte=(q*Q)/(4*pi*e0*m) #Constante que multiplica a las ecuaciones de las trayectorias
+
+#Definimos la función derivadas que queremos integrar
+def derivadas(var,t):
+	x, y, vx, vy= var[0], var[1], var[2], var[3]
+	ax=cte*(x/((x**2+y**2)**(3./2)))
+	ay=cte*(y/((x**2+y**2)**(3./2)))
 	return array([vx, vy, ax, ay])
-	
+
+#Creamos el vector de tiempos e introducimos las condiciones iniciales
 tiempos=linspace(0,100*zs,1000)
+x, vx, vy= 100*R, -(2*R)/zs, 0.0
+b=random.randint(0,200,(60,)) #Parámetro de impacto, es la distancia entre la dirección de la particula incidente y el centro de fuerzas, es decir, la y.
 
-#Condiciones iniciales
-x, y, vx, vy= 10*R, R, -2.e-7, 0.
-b=random.randint(0,10,(5,))
-
-#Bucle para meter todos los b
-'''
-for y in b:
-	trayectorias= odeint(derivadas, array([x, y, vx, vy]), tiempos)
+#Como b van a ser varios números, acemos un bucle for en el que para cada número se llame a la función odeint
+for k in b:
+	y=k*R/1000
+	trayectorias=odeint(derivadas,array([x, y, vx, vy]), tiempos)
 	xt, yt, vxt, vyt = trayectorias[:,0], trayectorias[:,1], trayectorias[:,2], trayectorias[:,3]
-	print xt
-	print yt
 	
-	#Dibujo de trayectorias
-	plt.plot(xt, yt,'b-')
+	#Dibujo de las trayectorias
+	plt.plot(xt, yt)
 	
 plt.plot(0,0,'ro')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.axis()
-plt.show()
-'''
-trayectorias= odeint(derivadas, array([x, y, vx, vy]), tiempos)
-xt, yt, vxt, vyt = trayectorias[:,0], trayectorias[:,1], trayectorias[:,2], trayectorias[:,3]
-print xt
-print "----------------------------"
-print yt
-
-plt.plot(xt, yt,'b-')	
-plt.plot(0,0,'ro')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.axis()
 plt.show()
 
 #Ángulo de dispersión
---
+++ TrabajoEuler.py
@@ -0,0 +1,52 @@
+#Experimento de Rutherford (Modo Euler)
+# -*- coding: utf-8 -*-
+from scipy.integrate import odeint
+from numpy import *
+import matplotlib.pyplot as plt
+from math import pi, atan
+
+#Parámetros del problema
+R, zs, qe, u, e0= 7.5e-14, 1.0e-21, 1.6e-19, 1.66e-27, 8.08542e-12
+
+#Datos de las cargas y masas
+Q=79*qe         #Carga del núclero de oro
+q=2*qe          #Carga de la partícula alfa (núcleo de Helio)
+m=4*u          #Masa de la partícula alfa 
+
+cte=(q*Q)/(4*pi*e0*m) #Constante que multiplica a las ecuaciones de las trayectorias
+
+#Definimos la función derivadas que queremos integrar
+def derivadas(x, y, vx, vy, t):
+	return cte*(x/((x**2+y**2)**(3./2))), cte*(y/((x**2+y**2)**(3./2)))
+	
+#Definimos el método de Euler, que es el que usaremos para integrar la función
+def euler(funcion, x, y, vx, vy, tiempos):
+	n=tiempos.size
+	xt, yt, vxt, vyt= zeros_like(tiempos), zeros_like(tiempos), zeros_like(tiempos), zeros_like(tiempos)
+	xt[0], yt[0], vxt[0], vyt[0]= x, y, vx, vy
+	print '\n    x    |    y     |    vx    |    vy    '
+	print '-----------------------------------------'
+	for k in range(1,n):
+		t=tiempos[k-1]
+		Dt=(tiempos[k]-tiempos[k-1])
+		ax, ay= funcion(x, y, vx, vy, t)
+		t, vx, vy, x, y= t+Dt, vx+ax*Dt, vy+ay*Dt, x+vx*Dt, y+vy*Dt
+		xt[k], yt[k], vxt[k], vyt[k]= x, y, vx, vy
+		print '%.2e | %.2e | %.2e | %.2e '%(x,y,vx,vy)
+	return xt, yt, vxt, vyt
+	
+#Creamos el vector de tiempos e introducimos las condiciones iniciales
+tiempos=linspace(0,100*zs,100)
+x, vx, vy= 100*R, -(2*R)/zs, 0.0
+b=random.randint(0,200,(10,)) #Parámetro de impacto, es la distancia entre la dirección de la particula incidente y el centro de fuerzas, es decir, la y.
+
+#
+for k in b:
+	y=k*R/1000
+	xt, yt, vxt, vyt=euler(derivadas, x, y, vx, vy, tiempos)
+	plt.plot(xt, yt)
+	
+plt.plot(0,0,'ro')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
